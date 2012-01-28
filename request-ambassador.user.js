e168ec4d0d2e8e322938994b530b111f551f1aab
Marlin Cremers <marlinc@mms-projects.net>
2012-01-28 18:28:42 +0100

    Made a gui class in the script

--- request-ambassador.user.js
+++ request-ambassador.user.js
@@ -3,16 +3,38 @@
 // @namespace     https://github.com/NPO-project/player-request
 // @description	  Vraagt een beschikbare ambassadeur aan
 // @include       http://nl*.tribalwars.nl/game.php*&mode=vacation
+// @require       http://code.jquery.com/jquery-1.5.1.js
 // ==/UserScript==
 
-var requester = {
-    interface: {
-        
-    },
-    debugging: {
-        log: function (text) {
-            console.log(text);
-        }
-    }
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
 }
-requester.debugging.log('Hello World!');
+
+addJQuery(main);
+
+function main() {
+    var requester = {
+        gui: {
+            initialize: function () {
+                requester.debugging.log('Initializing the interface class...');
+                this.area = $('td#content_value>table:eq(0)>tbody:eq(0)>tr:eq(0)>td:eq(1)');
+            },
+            area: null
+        },
+        debugging: {
+            log: function (text) {
+                console.log(text);
+            }
+        }
+    };
+    requester.debugging.log('Started the ambassador script');
+    requester.gui.initialize();
+    requester.debugging.log('Fully initialized the script');
+};
